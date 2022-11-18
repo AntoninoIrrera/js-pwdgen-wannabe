2cca91d548e6e55e0969e98c8095a3cf970277a6
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-18 15:12:16 +0100

    script js per generare e visualizzare la password generata

---
+++ java/myscript.js
@@ -0,0 +1,5 @@
+const userName = prompt("Inserisci il tuo nome");
+const userSurname = prompt("Inserisci il tuo cognome");
+const userColor = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("pwdgen").innerHTML = userName + userSurname + userColor + "22";
