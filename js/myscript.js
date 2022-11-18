63a8c6e5a923fd96d9c94ba7a7caca518c50595e
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-18 17:47:25 +0100

    migliorie estetiche

--- js/myscript.js
+++ js/myscript.js
@@ -2,4 +2,11 @@ const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const userColor = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById("pwdgen").innerHTML = userName + userSurname + userColor + "22";
+document.getElementById("name").innerHTML = "Il tuo nome è: " + userName;
+document.getElementById("surname").innerHTML = "Il tuo cognome è: " +  userSurname;
+document.getElementById("color").innerHTML = "Il tuo colore preferito è: " + userColor;
+
+
+
+
+document.getElementById("pwdgen").innerHTML = "La password generata è: " + userName + userSurname + userColor + "22";
